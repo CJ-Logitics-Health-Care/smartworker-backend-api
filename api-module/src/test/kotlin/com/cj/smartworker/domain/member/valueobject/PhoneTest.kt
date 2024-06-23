d811cb6ca0126b16840d79ed4b6a2982faf66618
Bangseungjae <tmdwo5825@naver.com>
2024-06-23 23:57:55 +0900

    Feature: Member Domain

--- api-module/src/test/kotlin/com/cj/smartworker/domain/member/valueobject/PhoneTest.kt
+++ api-module/src/test/kotlin/com/cj/smartworker/domain/member/valueobject/PhoneTest.kt
@@ -4,12 +4,12 @@ import com.cj.smartworker.domain.member.exception.MemberDomainException
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-class PhoneNumberTest {
+class PhoneTest {
     @Test
     fun `잘못된 휴대폰 번호를 넣은 경우 MemberDomainException 에외를 던진다`() {
         val invalidPhoneNumber = "010-1234-567p"
         assertThrows(MemberDomainException::class.java) {
-            PhoneNumber(invalidPhoneNumber)
+            Phone(invalidPhoneNumber)
         }.message.let {
             assertEquals("전화번호는 숫자와 '-'만 가능합니다.", it)
         }
@@ -19,7 +19,7 @@ class PhoneNumberTest {
     fun `정상적으로 입력한 경우`() {
         val phoneNumber = "010-1234-5678"
         assertDoesNotThrow {
-            PhoneNumber(phoneNumber)
+            Phone(phoneNumber)
         }
     }
 }
