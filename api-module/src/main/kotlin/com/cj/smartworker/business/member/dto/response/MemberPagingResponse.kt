570bc7a4d231ccaad177d49c17987889e229ebc5
Bangseungjae <tmdwo5825@naver.com>
2024-07-02 16:19:00 +0900

    Feature: Admin 회원 페이징 조회 API

---
+++ api-module/src/main/kotlin/com/cj/smartworker/business/member/dto/response/MemberPagingResponse.kt
@@ -0,0 +1,29 @@
+package com.cj.smartworker.business.member.dto.response
+
+import com.cj.smartworker.domain.member.valueobject.Authority
+import com.cj.smartworker.domain.member.valueobject.Gender
+import com.fasterxml.jackson.annotation.JsonFormat
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+@Schema(description = "커서 페이징 조회 회원 정보 응답")
+data class MemberPagingResponse(
+    @Schema(description = "회원 ID")
+    val memberId: Long,
+    @Schema(description = "성별")
+    val gender: Gender,
+    @Schema(description = "권한")
+    val authorities: Set<Authority>,
+    @Schema(description = "태어난 연도")
+    val year: Int,
+    @Schema(description = "태어난 월")
+    val month: Int,
+    @Schema(description = "태어난 일")
+    val day: Int,
+    @Schema(description = "생성일시")
+    @JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "yyyy-MM-dd HH:mm:ss"
+    )
+    val createdAt: LocalDateTime,
+)
---
+++ api-module/src/main/kotlin/com/cj/smartworker/business/member/service/CursorPagingMemberService.kt
@@ -0,0 +1,29 @@
+package com.cj.smartworker.business.member.service
+
+import com.cj.smartworker.business.common.dto.request.CursorRequest
+import com.cj.smartworker.business.common.dto.response.CursorResultResponse
+import com.cj.smartworker.business.member.dto.response.MemberPagingResponse
+import com.cj.smartworker.business.member.port.`in`.CursorPagingMemberUseCase
+import com.cj.smartworker.business.member.port.out.HasNextMemberPort
+import com.cj.smartworker.business.member.port.out.PagingMemberPort
+import com.cj.smartworker.domain.member.valueobject.MemberId
+import org.springframework.stereotype.Service
+
+@Service
+internal class CursorPagingMemberService(
+    private val pagingMemberPort: PagingMemberPort,
+    private val hasNextMemberPort: HasNextMemberPort,
+): CursorPagingMemberUseCase {
+    override fun findMembers(cursorRequest: CursorRequest): CursorResultResponse<MemberPagingResponse> {
+        pagingMemberPort.pagingMembers(cursorRequest).let { members ->
+            val lastIndex = if (members.isEmpty()) null else members.last().memberId
+
+            val hasNext = if (lastIndex != null){
+                hasNextMemberPort.hasNext(MemberId(members.last().memberId))
+            } else {
+                false
+            }
+            return CursorResultResponse(members, hasNext, lastIndex)
+        }
+    }
+}
