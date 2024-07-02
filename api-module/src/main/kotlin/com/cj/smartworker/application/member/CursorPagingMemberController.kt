f6bfef792486a03b255400ef2092db44c397beef
Bangseungjae <tmdwo5825@naver.com>
2024-07-02 16:21:24 +0900

    Docs: Admin 회원 페이징 조회 API

--- api-module/src/main/kotlin/com/cj/smartworker/application/member/CursorPagingMemberController.kt
+++ api-module/src/main/kotlin/com/cj/smartworker/application/member/CursorPagingMemberController.kt
@@ -7,6 +7,7 @@ import com.cj.smartworker.business.common.dto.response.CursorResultResponse
 import com.cj.smartworker.business.member.dto.response.MemberPagingResponse
 import com.cj.smartworker.business.member.port.`in`.CursorPagingMemberUseCase
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
@@ -23,7 +24,14 @@ import org.springframework.web.bind.annotation.RestController
 internal class CursorPagingMemberController(
     private val cursorPagingMemberUseCase: CursorPagingMemberUseCase
 ) {
-    @Operation(summary = "직원 전체 커서 페이징 조회", description = "직원 전체 커서 페이징 조회를 진행합니다.\nAdmin만 가능합니다.")
+    @Operation(
+        summary = "직원 전체 커서 페이징 조회",
+        description = "직원 전체 커서 페이징 조회를 진행합니다.\nAdmin만 가능합니다.",
+        parameters = [
+            Parameter(name = "next", description = "다음 페이지 번호", required = false),
+            Parameter(name = "size", description = "페이지 사이즈", required = false),
+        ]
+    )
     @ApiResponse(responseCode = "200", description = "조회 성공")
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/cursor-paging")
