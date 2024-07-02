547cf16be08b4ad2516b60111891842921432a25
Seungjae Bang <tmdwo5825@naver.com>
2024-07-02 16:20:21 +0900

    Feature: Admin 회원 페이징 조회 API (#20)

---
+++ api-module/src/main/kotlin/com/cj/smartworker/dataaccess/member/mapper/MemberPagingResponseMapper.kt
@@ -0,0 +1,16 @@
+package com.cj.smartworker.dataaccess.member.mapper
+
+import com.cj.smartworker.business.member.dto.response.MemberPagingResponse
+import com.cj.smartworker.dataaccess.member.entity.MemberJpaEntity
+
+fun MemberJpaEntity.toMemberPagingResponse(): MemberPagingResponse = let {
+    MemberPagingResponse(
+        memberId = it.id!!,
+        createdAt = it.createdAt,
+        gender = it.gender,
+        authorities = it.authorities.map { authority -> authority.authority }.toSet(),
+        year = it.year,
+        month = it.month,
+        day = it.day,
+    )
+}
