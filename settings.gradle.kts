544b4d09f8a53e9c161efaa2c37911233503391f
Bangseungjae <tmdwo5825@naver.com>
2024-07-08 01:04:33 +0900

    Refactoring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "smartworker-backend-api"
 
 include(":api-module")
 include(":save-health-rate-module")
-include(":alarm-module")
 include(":document-db-module")
 include(":common-messaging-module")
 include(":common-messaging-module:kafka-config-data")
