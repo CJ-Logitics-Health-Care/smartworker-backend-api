b25e43274e6f37ecd641aa3e947dfe2853f12633
Bangseungjae <tmdwo5825@naver.com>
2024-07-18 14:26:05 +0900

    Feature: 본인 신고이력 조회 API

--- api-module/src/main/kotlin/com/cj/smartworker/business/fcm/port/out/FindEmergencyReportPort.kt
+++ api-module/src/main/kotlin/com/cj/smartworker/business/fcm/port/out/FindEmergencyReportPort.kt
@@ -1,8 +1,11 @@
 package com.cj.smartworker.business.fcm.port.out
 
 import com.cj.smartworker.business.fcm.dto.response.EmergencyReportDto
+import com.cj.smartworker.domain.member.entity.Member
 import java.time.LocalDateTime
 
 interface FindEmergencyReportPort {
     fun findReport(start: LocalDateTime, end: LocalDateTime): List<EmergencyReportDto>
+
+    fun findReport(member: Member): List<EmergencyReportDto>
 }
