3b1b5162b649e20bafb133c5221150ee5bf9ad84
Seungjae Bang <tmdwo5825@naver.com>
2024-07-18 21:46:13 +0900

    Feature: 특정 회원 신고 내역 조회 API (#45)
    * Feature: 특정 회원 신고 내역 조회 API
    
    * Docs: 특정 회원 신고 내역 조회 API

--- api-module/src/main/kotlin/com/cj/smartworker/business/fcm/port/in/FindEmergencyReportUseCase.kt
+++ api-module/src/main/kotlin/com/cj/smartworker/business/fcm/port/in/FindEmergencyReportUseCase.kt
@@ -2,10 +2,13 @@ package com.cj.smartworker.business.fcm.port.`in`
 
 import com.cj.smartworker.business.fcm.dto.response.EmergencyReportDto
 import com.cj.smartworker.domain.member.entity.Member
+import com.cj.smartworker.domain.member.valueobject.MemberId
 import java.time.LocalDateTime
 
 interface FindEmergencyReportUseCase {
     fun findReport(start: LocalDateTime, end: LocalDateTime): List<EmergencyReportDto>
 
     fun findReport(member: Member): List<EmergencyReportDto>
+
+    fun findReport(memberId: MemberId): List<EmergencyReportDto>
 }
