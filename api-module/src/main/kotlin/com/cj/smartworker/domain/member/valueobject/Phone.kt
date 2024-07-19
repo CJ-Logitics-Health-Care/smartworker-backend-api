2c19ad01f7fda1f6aebb428c0fe694f1e56af2f9
Bangseungjae <tmdwo5825@naver.com>
2024-07-20 08:49:24 +0900

    Fix: 전화번호 검증 로직 변경

--- api-module/src/main/kotlin/com/cj/smartworker/domain/member/valueobject/Phone.kt
+++ api-module/src/main/kotlin/com/cj/smartworker/domain/member/valueobject/Phone.kt
@@ -4,11 +4,12 @@ package com.cj.smartworker.domain.member.valueobject
 value class Phone(val phone: String) {
     init {
         require(phone.matches(PHONE_REGEX)) {
-            "전화번호는 숫자와 '-'만 가능합니다."
+            "전화번호는 숫자와 '-'만 가능합니다. Ex: 010-1234-1234"
         }
     }
 
     companion object {
-        private val PHONE_REGEX = "^[0-9-]+$".toRegex()
+        private val PHONE_REGEX = "^\\d{3}-\\d{4}-\\d{4}$".toRegex()
+//        private val PHONE_REGEX = "^[0-9-]+$".toRegex()
     }
 }
