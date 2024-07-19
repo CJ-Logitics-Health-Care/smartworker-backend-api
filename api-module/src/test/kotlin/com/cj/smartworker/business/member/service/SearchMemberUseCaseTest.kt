c3ea253a2e439a535ed8d564d3a299a678f5fb68
Seungjae Bang <tmdwo5825@naver.com>
2024-07-19 18:36:47 +0900

    Fix: 회원 상세 조회에 임계치 추가 (#51)
    * Fix
    
    * Fix: 상세 조회에 임계치 추가

--- api-module/src/test/kotlin/com/cj/smartworker/business/member/service/SearchMemberUseCaseTest.kt
+++ api-module/src/test/kotlin/com/cj/smartworker/business/member/service/SearchMemberUseCaseTest.kt
@@ -2,6 +2,8 @@ package com.cj.smartworker.business.member.service
 
 import com.cj.smartworker.business.member.port.`in`.SearchMemberUseCase
 import com.cj.smartworker.business.member.port.`in`.SignupUseCase
+import com.cj.smartworker.business.member.util.MaskingUtil
+import com.cj.smartworker.domain.member.valueobject.EmployeeName
 import com.cj.smartworker.domain.member.valueobject.LoginId
 import com.cj.smartworker.testbase.IntegrationTestBase
 import com.cj.smartworker.fixture.MemberFixture
@@ -19,6 +21,6 @@ class SearchMemberUseCaseTest @Autowired constructor(
         signupUseCase.signup(signupCommand)
         val loginId = signupCommand.loginId
         val memberResponse = searchMemberUseCase.searchByLoginId(LoginId(loginId))
-        assertEquals(signupCommand.employeeName, memberResponse!!.name)
+        assertEquals(MaskingUtil.maskEmployeeName(EmployeeName(signupCommand.employeeName)), memberResponse.value[0].employeeName)
     }
 }
